3f5ac869d6dfdfec2f2812565658b2472b0faaa1
Fernando Daciuk <f.daciuk@gmail.com>
2015-09-09 00:26:14 -0300

    Improve main

--- webpack/react/main.js
+++ webpack/react/main.js
@@ -1,8 +1,4 @@
 import React from 'react';
 import Hello from './component.jsx';
 
-main();
-
-function main() {
-  return React.render(<Hello />, document.querySelector('[data-js="main"]'));
-}
+React.render(<Hello />, document.querySelector('[data-js="main"]'));
