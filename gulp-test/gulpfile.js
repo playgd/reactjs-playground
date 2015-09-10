3d0865c1c6152b8bed4ac3cc34ed4ffdbaabe10c
Fernando Daciuk <f.daciuk@gmail.com>
2015-09-10 08:58:47 -0300

    Gulp + browserify + es6 + babel is working

--- gulp-test/gulpfile.js
+++ gulp-test/gulpfile.js
@@ -1,10 +1,39 @@
-import gulp from 'gulp';
-import browserify from 'browserify';
-import source from 'vinyl-source-stream';
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babel = require('babelify');
 
-gulp.task('default', () => {
-  var b = browserify('./js/main.js').bundle();
-    return b
-    .pipe(source('./js/main.js'))
-    .pipe(gulp.dest('public'));
-});
+function compile(watch) {
+  var bundler = watchify(browserify('./js/main.js', { debug: true }).transform(babel));
+
+  function rebundle() {
+    bundler.bundle()
+      .on('error', function(err) { console.error(err); this.emit('end'); })
+      .pipe(source('build.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./build'));
+  }
+
+  if (watch) {
+    bundler.on('update', function() {
+      console.log('-> bundling...');
+      rebundle();
+    });
+  }
+
+  rebundle();
+}
+
+function watch() {
+  return compile(true);
+};
+
+gulp.task('build', function() { return compile(); });
+gulp.task('watch', function() { return watch(); });
+
+gulp.task('default', ['watch']);
--- gulp-test/js/main.js
+++ gulp-test/js/main.js
@@ -2,5 +2,5 @@
 
 import React from 'react';
 import Hello from './component.js';
-
+console.log('main');
 React.render(<Hello />, document.querySelector('[data-js="main"]'));
